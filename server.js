b4c42fc3b6155d55ea934af8760bd7062b9f7ab0
Jordan Olguin <jordanolguin@iMac.local>
2023-06-22 23:06:49 -0600

    added ADD inquirer prompts

--- server.js
+++ server.js
@@ -73,20 +73,118 @@ function init() {
     });
 }
 
-function viewAllEmployees()
+function viewAllEmployees() {
+  // Execute a db.query to fetch all employees
+  // Display the employee data in a formatted table
+  // Call init() to prompt the user with the menu options again
+}
 
-function addEmployee()
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "What is the employee's first name?",
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "What is the employee's last name?",
+      },
+      {
+        type: "list",
+        name: "employeeDepartment",
+        message: "What is the employee's role?",
+        choices: [
+          "Sales Lead",
+          "Salesperson",
+          "Lead Engineer",
+          "Software Engineer",
+          "Account Manager",
+          "Accountant",
+          "Legal Team Lead",
+          "Lawyer",
+        ],
+      },
+      {
+        type: "list",
+        name: "employeeManager",
+        message: "Who is the employee's manager?",
+        choices: [
+          "None",
+          "John Doe",
+          "Mike Chan",
+          "Ashley Rodriguez",
+          "Kevin Tupik",
+          "Kunal Singh",
+          "Malia Brown",
+          "Sarah Lourd",
+          "Tom Allen",
+        ],
+      },
+    ])
+    .then((answers) => {
+      //execute db.query to add the employee with the provided info
+      //call init() to prompt the menu options again
+    });
+}
 
-function updateEmployeeRole()
+function updateEmployeeRole() {}
 
-function viewAllRoles()
+function viewAllRoles() {
+  // Execute a db.query to fetch all roles
+  // Display the role data in a formatted table
+  // Call init() to prompt the user with the menu options again
+}
 
-function addRole()
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "roleTitle",
+        message: "What is the name of the role?",
+      },
+      {
+        type: "input",
+        name: "roleSalary",
+        message: "What is the salary of the role?",
+      },
+      {
+        type: "list",
+        name: "roleDepartment",
+        message: "Which department does the role belong to?",
+        choices: ["Sales", "Engineering", "Finance", "Legal"],
+      },
+    ])
+    .then((answers) => {
+      //execute db.query to add the role with the provided title, salary, and department
+      //call init() to prompt the menu options again
+    });
+}
 
-function viewAllDepartments()
+function viewAllDepartments() {
+  // Execute a db.query to fetch all departments
+  // Display the department data in a formatted table
+  // Call init() to prompt the user with the menu options again
+}
 
-function addDepartment()
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "departmentName",
+        message: "What is the name of the department?",
+      },
+    ])
+    .then((answers) => {
+      //execute db.query to add the department with the provided name
+      //call init() to prompt the menu options again
+    });
+}
 
-function exit()
+function exit() {}
 
 init();
