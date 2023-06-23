29b062dbf628992647b96496ba36f66458998558
Jordan Olguin <jordanolguin@iMac.local>
2023-06-22 22:40:25 -0600

    added initial menu, switch statement with server connection bug

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
@@ -16,6 +17,76 @@ db.connect((err) => {
     return;
   }
   console.log("Connected to the employee_tracker_db database.");
-  console.log("CLI app is running.");
   console.log(`Using PORT: ${PORT}`);
 });
+
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "menuChoice",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Departments",
+          "Add Department",
+          "Exit",
+        ],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.menuChoice) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+        case "View All Roles":
+          viewAllRoles();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "View All Departments":
+          viewAllDepartments();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
+        case "Exit":
+          exit();
+          break;
+        default:
+          console.log("Invalid choice. Please try again.");
+          init();
+          break;
+      }
+    });
+}
+
+function viewAllEmployees()
+
+function addEmployee()
+
+function updateEmployeeRole()
+
+function viewAllRoles()
+
+function addRole()
+
+function viewAllDepartments()
+
+function addDepartment()
+
+function exit()
+
+init();
